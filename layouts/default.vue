7f56bee1c2104f5a7a85603d33dd503b0b63cc9e
Spandan Barve <spandan567@gmail.com>
2023-02-22 00:09:20 +0530

    Fixed Z-index of maintainance message

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,5 @@
   <Navbar />
   <slot />
   <Footer />
-  <div class="fixed top-0 left-0 h-screen w-screen flex items-center justify-center italic">Website under maintainance</div>
+  <div class="fixed top-0 left-0 h-screen w-screen flex items-center justify-center italic z-[9999999999]">Website under maintainance</div>
 </template>
