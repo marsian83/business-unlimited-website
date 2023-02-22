3d3beb2772398b8df19f5ecf3097a4bc16c72b6d
Spandan Barve <spandan567@gmail.com>
2023-02-22 15:07:14 +0530

    maintainance complete

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,4 @@
   <Navbar />
   <slot />
   <Footer />
-  <div class="fixed top-0 left-0 h-screen w-screen flex items-center justify-center italic bg-secondary z-[9999999999]">Website under maintainance</div>
 </template>
