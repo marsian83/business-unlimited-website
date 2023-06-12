fe18314b070bc115fea5a1745c1f285f4a8723e4
Spandan Barve <spandan567@gmail.com>
2023-06-12 15:54:39 +0530

    changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,13 @@
   <Preloader />
   <Navbar />
   <slot />
-  <div
+  <!-- <div
     class="fixed top-0 left-0 z-[999999] w-full h-full bg-bacground text-primary text-center flex items-center justify-center text-3xl italic bg-opacity-70 backdrop-blur-lg"
   >
     <p class="bg-white p-2 rounded-lg bg-opacity-70 font-medium">
       Development Preview has expired
     </p>
-  </div>
+  </div> -->
 </template>
 
 <style>
